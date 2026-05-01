7b9e710f0f15f675d2e00f7e8ff25f8d54e5f884
LockeCoder <160626087+LockeCoder@users.noreply.github.com>
2026-05-01 03:32:38 -0400

    Refactor questionable code and improve comments

--- QuestionableCode.cpp
+++ QuestionableCode.cpp
@@ -1,37 +1,43 @@
-// QuestionableCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// QuestionableCode.cpp : This file contains the 'main' function.
+// Program execution begins and ends there.
 
 #include <cassert>
 #include <iostream>
-#include <numeric>
 #include <set>
 #include <vector>
 
-// TODO: You are going to compare the warnings & errors between Visual Studio and CppCheck.
-//  Make sure you are using CppCheck 2.1 or greater
+// TODO: Compare the warnings and errors between Visual Studio and CppCheck.
+// Make sure you are using CppCheck 2.1 or greater.
 //
-//  1. Create a Visual Studio Console project with this file as the only file
-//  2. Compile it and check the warning and errors
-//  3. Create a CppCheck project to analyze a Visual Studio project with this file.
-//  4. In CppCheck Edit / Preferences / General Tab: Set Display error id in column "Id", Enable inline suppressions, and Check for inconclusive errors also
-//  5. In CppCheck View, Select Check All to ensure all types of checks are enabled
-//  6. In CppCheck Analyze, set the C++ Standard to C++17, and Enforce C++
-//  7. Make sure to run the analysis
-//  8. Save the results to a file (XML format)
-//  9. Take a screen shot of the Visual Studio Error list (all errors, warnings, and messages)
-//  10. Identify all messages from CppCheck NOT identified in Visual Studio.
-//      For each message not in both:
-//        Identify the risk as: RISK or NOT RISK
-//        Identify which system (Visual Studio or CppCheck) found the issue
-//        Provide a couple of sentences describing the issue found
+// 1. Create a Visual Studio Console project with this file as the only file.
+// 2. Compile it and check the warnings and errors.
+// 3. Create a CppCheck project to analyze a Visual Studio project with this file.
+// 4. In CppCheck Edit / Preferences / General Tab:
+//    - Set "Display error id in column Id"
+//    - Enable inline suppressions
+//    - Check for inconclusive errors also
+// 5. In CppCheck View, select Check All to ensure all check types are enabled.
+// 6. In CppCheck Analyze, set the C++ Standard to C++17 and enforce C++.
+// 7. Run the analysis.
+// 8. Save the results to a file in XML format.
+// 9. Take a screenshot of the Visual Studio Error List.
+// 10. Identify all CppCheck messages not identified in Visual Studio.
+//     For each message not in both:
+//     - Identify the risk as RISK or NOT RISK.
+//     - Identify which system found the issue.
+//     - Provide a short explanation of the issue.
 
 class C
 {
     std::set<int> typedefs;
+
     bool is_type(int type) const
     {
         if (typedefs.find(type) != typedefs.end())
+        {
             return is_type(type); // BUG: endless recursion
+        }
+
         return false;
     }
 };
@@ -39,7 +45,10 @@ class C
 class A
 {
     int x;
-    A(const A& other) {}
+
+    A(const A& other)
+    {
+    }
 };
 
 class MySpecialType
@@ -62,13 +71,17 @@ void foo(int** a)
 void work_with_arrays(int count)
 {
     int buf[10];
+
     if (count == 1000)
-        buf[count] = 0; // <- ERROR
+    {
+        buf[count] = 0; // ERROR: out-of-bounds write
+    }
 }
 
 void do_something_useless()
 {
     int sum = 0;
+
     for (auto i = 0; i < 1000; ++i)
     {
         sum += i;
@@ -83,15 +96,20 @@ void vector_test()
     items.push_back(1);
     items.push_back(2);
     items.push_back(3);
+
     std::vector<int>::iterator iter;
-    for (iter = items.begin(); iter != items.end(); ++iter) {
-        if (*iter == 2) {
+
+    for (iter = items.begin(); iter != items.end(); ++iter)
+    {
+        if (*iter == 2)
+        {
             items.erase(iter);
         }
     }
 }
 
 int a;
+
 bool my_function()
 {
     a = 1 + 2;
@@ -100,12 +118,19 @@ bool my_function()
 
 struct Token
 {
-    Token* next() { return nullptr; }
+    Token* next()
+    {
+        return nullptr;
+    }
 };
 
 int foo(Token* tok)
 {
-    while (tok);
+    while (tok)
+    {
+        // Intentional issue for static analysis demonstration.
+    }
+
     tok = tok->next();
 
     return 0;
@@ -120,12 +145,11 @@ int main()
 
     std::cout << "Welcome to the Questionable Code Test!" << std::endl;
 
-    //do_something_useless();
+    // do_something_useless();
 
     work_with_arrays(10);
 
     assert(z = 2);
-
     assert(my_function() == 3);
 
     try
@@ -133,11 +157,11 @@ int main()
         int x = 5;
         int y = 5;
         int z = 5;
+
         std::cout << "x + y + z = " << (x + y + z) << std::endl;
     }
     catch (...)
     {
-
     }
 
     int* c;
@@ -149,5 +173,5 @@ int main()
     std::cout << "myobject.MyVal = " << myobject.MyVal << std::endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu.
+// Debug program: F5 or Debug > Start Debugging menu.
